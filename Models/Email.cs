1782c7cfb48e2cd46756e446ec50ae0c0b6c123b
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-04-18 22:04:08 -0400

    Add files via upload

---
+++ Models/Email.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MyFirstAPI.Models
+{
+	public class Email
+	{
+		public int EmailId { get; set; }
+		public string? EmailAddres { get; set; }
+		public bool IsSubscribed { get; set; }
+	}
+}
+
